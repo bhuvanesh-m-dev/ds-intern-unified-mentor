7141549a919defea80812f66f2ba498d1c67a52f
BHUVANESH M <bhuvaneshm.developer@gmail.com>
2025-07-01 12:54:09 +0530

    Fix: Resolve bug related to absolute file directory path date_added.py

--- netflix/statistics/USA/date_added.py
+++ netflix/statistics/USA/date_added.py
@@ -3,7 +3,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from scipy import stats
 
-data = pd.read_csv("/home/bhuvanesh-m-ubuntu/Desktop/Netflix/files/cleaned_netflix.csv")
+data = pd.read_csv("netflix/files/cleaned_netflix.csv")
 
 # Filter for USA only
 data_usa = data[data['country'].str.contains('United States', na=False)]
