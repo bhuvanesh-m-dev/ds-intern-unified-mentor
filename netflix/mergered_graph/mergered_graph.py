04b7ee6c5b7cbe3fa0e3fa782e1165deffd8267d
BHUVANESH M <bhuvaneshm.developer@gmail.com>
2025-07-01 12:44:53 +0530

    Fix: Resolve bug related to absolute file directory path mergered_graph.py

--- netflix/mergered_graph/mergered_graph.py
+++ netflix/mergered_graph/mergered_graph.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-data = pd.read_csv("/home/bhuvanesh-m-ubuntu/Desktop/Netflix/files/cleaned_netflix.csv")
+data = pd.read_csv("netflix/files/cleaned_netflix.csv")
 
 # Merge both Movies and TV Shows per year in a single graph
 print('Graph for total number of Movies and TV Shows released per year in Netflix OTT')
