f464a7518ffb642b877f421bae1fe8d547277978
BHUVANESH M <bhuvaneshm.developer@gmail.com>
2025-07-04 06:22:59 +0530

    Update create_data.py

--- deviation/create_data.py
+++ deviation/create_data.py
@@ -1,1 +1,6 @@
-
+import random
+import pickle
+data = [random.randint(1, 1000) for _ in range(500)]
+sample_data = random.sample(data, 50)
+with open('data.dat', 'wb') as f:
+    pickle.dump({'data': data, 'sample_data': sample_data}, f)
