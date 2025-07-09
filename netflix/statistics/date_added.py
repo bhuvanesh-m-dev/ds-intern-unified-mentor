25031e34d9434d4fef3facc0a753bc93759ee121
BHUVANESH M <bhuvaneshm.developer@gmail.com>
2025-07-09 17:59:45 +0530

    Update date_added.py

--- netflix/statistics/date_added.py
+++ netflix/statistics/date_added.py
@@ -9,12 +9,10 @@ data = pd.read_csv("netflix/files/cleaned_netflix.csv")
 data['date_added'] = pd.to_datetime(data['date_added'], errors='coerce')
 data = data.dropna(subset=['date_added'])
 
-# Extract year, month, and day
 data['year_added'] = data['date_added'].dt.year
 data['month_added'] = data['date_added'].dt.month
 data['day_added'] = data['date_added'].dt.day
 
-# statistics for year_added
 mean_year = data['year_added'].mean()
 median_year = data['year_added'].median()
 mode_year = data['year_added'].mode()[0] if not data['year_added'].mode().empty else None
@@ -23,7 +21,6 @@ print(f"Mean year: {mean_year}")
 print(f"Median year: {median_year}")
 print(f"Mode year: {mode_year}")
 
-# Visualize the distribution 
 plt.figure(figsize=(10,6))
 sns.histplot(data['year_added'], bins=20, kde=False, color='skyblue')
 plt.title('Distribution of Content Added to Netflix by Year')
