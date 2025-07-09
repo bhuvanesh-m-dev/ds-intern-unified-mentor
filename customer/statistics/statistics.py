86719084c234f5245f826b73b574946911a61c8f
BHUVANESH M <bhuvaneshm.developer@gmail.com>
2025-07-09 16:40:41 +0530

    Update statistics.py

--- customer/statistics/statistics.py
+++ customer/statistics/statistics.py
@@ -1,1 +1,9 @@
-
+import pandas as pd 
+df = pd.read_csv('data.csv') 
+ages = df['Customer Age'] 
+mean_age = ages.mean() 
+median_age = ages.median() 
+mode_age = ages.mode()[0] 
+print(f"Mean Customer Age: {mean_age:.2f}") 
+print(f"Median Customer Age: {median_age:.2f}") 
+print(f"Mode Customer Age: {mode_age:.2f}")
