c749062d2bb6b93590339fa8679439c1163d1eb0
BHUVANESH M <bhuvaneshm.developer@gmail.com>
2025-07-09 17:59:32 +0530

    Update date_added.py

--- netflix/statistics/USA/date_added.py
+++ netflix/statistics/USA/date_added.py
@@ -5,18 +5,15 @@ from scipy import stats
 
 data = pd.read_csv("netflix/files/cleaned_netflix.csv")
 
-# Filter for USA only
 data_usa = data[data['country'].str.contains('United States', na=False)]
 
 data_usa['date_added'] = pd.to_datetime(data_usa['date_added'], errors='coerce')
 data_usa = data_usa.dropna(subset=['date_added'])
 
-# Extract year, month, and day
 data_usa['year_added'] = data_usa['date_added'].dt.year
 data_usa['month_added'] = data_usa['date_added'].dt.month
 data_usa['day_added'] = data_usa['date_added'].dt.day
 
-# statistics for year_added
 mean_year = data_usa['year_added'].mean()
 median_year = data_usa['year_added'].median()
 mode_year = data_usa['year_added'].mode()[0] if not data_usa['year_added'].mode().empty else None
@@ -25,7 +22,6 @@ print(f"Mean year (USA): {mean_year}")
 print(f"Median year (USA): {median_year}")
 print(f"Mode year (USA): {mode_year}")
 
-# Visualize the distribution 
 plt.figure(figsize=(10,6))
 sns.histplot(data_usa['year_added'], bins=20, kde=False, color='skyblue')
 plt.title('Distribution of Content Added to Netflix by Year (USA)')
