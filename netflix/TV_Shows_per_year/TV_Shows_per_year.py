91bf54c13c5fbdb8615e94019f569d2872c412a5
BHUVANESH M <bhuvaneshm.developer@gmail.com>
2025-07-09 16:58:53 +0530

    Update TV_Shows_per_year.py

--- netflix/TV_Shows_per_year/TV_Shows_per_year.py
+++ netflix/TV_Shows_per_year/TV_Shows_per_year.py
@@ -5,12 +5,9 @@ import matplotlib.pyplot as plt
 
 data = pd.read_csv("netflix/files/cleaned_netflix.csv")
 
-# Graph for total number of TV Shows (series) released per year
 print('Graph for total number of TV Shows (series) released per year in Netflix OTT')
-# Filter for years between 1900 and 2025
 filtered_df = data[(data['release_year'] >= 1900) & (data['release_year'] <= 2025)]
 
-# TV Shows per year
 tv_per_year = filtered_df[filtered_df['type'] == 'TV Show'].groupby('release_year').size().reset_index(name='total_tv_shows')
 tv_per_year = tv_per_year.sort_values('release_year')
 
@@ -22,7 +19,6 @@ plt.ylabel('Total TV Shows Released', fontsize=14)
 plt.title('Total Number of TV Shows Released per Year (1900–2025)', fontsize=16, fontweight='bold')
 plt.legend(fontsize=12)
 
-# Adjust x-axis ticks: every 2 years
 plt.xticks(ticks=range(int(tv_per_year['release_year'].min()), int(tv_per_year['release_year'].max())+1, 2), rotation=45, ha='right')
 
 plt.tight_layout()
